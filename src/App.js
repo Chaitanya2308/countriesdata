90064edf8ab01b0e4c59c5df9279a5d746f98ec1
Chaitanya2308 <chaitanyaguntialli1@gmail.com>
2021-02-16 21:36:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    getAllCountries();
+
+  }, [])
+
+  const getAllCountries = async () => {
+    var res = await fetch('http://localhost:8000/countries')
+    var resData = await res.json()
+    console.log(resData)
+    setCountries(resData)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+      <h1 className="text-center text-primary m-5">Fetching Countires Api data Using Node</h1>
+         <div className="row ">
+      {countries.map(ele =>
+      (
+     
+        <div class="card col-4 " >
+          <img src={ele.flag} class="card-img-top img-thumbnail" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{ele.name}</h5>
+            {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Capital: {ele.capital}</li>
+            <li class="list-group-item">Population: {ele.population}</li>
+          </ul>
+        </div>
+  
+      )
+      )}
+            </div>
+
     </div>
   );
 }
